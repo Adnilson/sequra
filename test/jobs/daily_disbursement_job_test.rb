4bf34c41fd130264196bd080559b9e3abab5a812
Adnilson <adnilson.delgado@outlook.com>
2024-01-21 17:50:05 +0000

    Add simple test to check the minimum fee when on the first disbursement of the month

--- test/jobs/daily_disbursement_job_test.rb
+++ test/jobs/daily_disbursement_job_test.rb
@@ -1,29 +1,31 @@
 require 'test_helper'
 
 class DailyDisbursementJobTest < ActiveJob::TestCase
+  include ActiveSupport::Testing::TimeHelpers
+
   def setup
-    merchant = Merchant.create(
+    @merchant = Merchant.create(
       reference: "jonas_brothers_band",
       email: "jonas_brothers_band@hotmail.com",
       live_on: "2023-03-21",
       disbursement_frequency: "DAILY",
-      minimum_monthly_fee: 0.0
+      minimum_monthly_fee: 29.99
     )
 
     Order.create(
-      merchant_reference: merchant.reference,
+      merchant_reference: @merchant.reference,
       amount: 33.00
     )
     Order.create(
-      merchant_reference: merchant.reference,
+      merchant_reference: @merchant.reference,
       amount: 33.04
     )
     Order.create(
-      merchant_reference: merchant.reference,
+      merchant_reference: @merchant.reference,
       amount: 63.22
     )
     Order.create(
-      merchant_reference: merchant.reference,
+      merchant_reference: @merchant.reference,
       amount: 33.04
     )
   end
@@ -32,6 +34,7 @@ class DailyDisbursementJobTest < ActiveJob::TestCase
     Merchant.delete_all
     Order.delete_all
     Disbursement.delete_all
+    MinimumMonthlyFee.delete_all
   end
 
   test "disbursement is created" do
@@ -47,5 +50,15 @@ class DailyDisbursementJobTest < ActiveJob::TestCase
     assert_equal 1.59, disbursement.fee
     assert_equal "jonas_brothers_band", disbursement.merchant_reference
     assert_equal disbursement.reference, Order.find_by(merchant_reference: disbursement.merchant_reference).disbursement_reference
+  end
+
+  test "check minimum monthly fee" do
+    travel_to Date.new(2024, 01, 01) do
+      perform_enqueued_jobs do
+        DailyDisbursementJob.perform_later
+      end
+
+      assert_equal @merchant.minimum_monthly_fee, MinimumMonthlyFee.first.amount
+    end
   end
 end
